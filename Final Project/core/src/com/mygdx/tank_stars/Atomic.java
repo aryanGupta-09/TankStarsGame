146daf9cc45946f8629a6167a35333eff41f9ec7
Aryan Gupta <96881807+DevilCoder09@users.noreply.github.com>
2022-11-14 00:58:27 +0530

    Add files via upload

--- Final Project/core/src/com/mygdx/tank_stars/Atomic.java
+++ Final Project/core/src/com/mygdx/tank_stars/Atomic.java
@@ -1,7 +1,16 @@
 package com.mygdx.tank_stars;
 
-public class Atomic {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 
+import java.util.ArrayList;
 
+public class Atomic extends Tank {
+
+    Atomic(){
+        tankImage_l = new Texture(Gdx.files.internal("atomic_l.png"));
+        tankImage_r = new Texture(Gdx.files.internal("atomic_r.png"));
+        weapons = new ArrayList<>(2);
+    }
 
 }
